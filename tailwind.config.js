89e595a357b130219d62a779487eeefd8f1e7e77
Phil Lennon <me@phil.dev>
2020-08-10 14:16:02 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    enabled: true,
+    content: ['src/**/*.njk', 'src/**/*.js'],
+  }
+}
